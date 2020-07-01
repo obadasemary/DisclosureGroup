e23cc902bbb88f64cda18542a7c3568ce7f7c6ca
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-02 00:25:30 +0300

    Initial Commit

---
+++ DisclosureGroup/DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  DisclosureGroupApp.swift
+//  DisclosureGroup
+//
+//  Created by Abdelrahman Mohamed on 2.07.2020.
+//
+
+import SwiftUI
+
+@main
+struct DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
