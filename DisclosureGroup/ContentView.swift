512305d224302508566c89f11b3711a43502eb2f
Abdelrahman Mohamed <obada.semary@gmail.com>
2020-07-02 00:51:52 +0300

    Use DisclosureGroup
    DisclosureGroup with animals and fruits list

--- DisclosureGroup/ContentView.swift
+++ DisclosureGroup/ContentView.swift
@@ -8,8 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var animalsExpanded: Bool = true
+    @State private var fruitsExpanded: Bool = false
+    let animals = ["🦧", "🐘", "🐬", "🐑"]
+    let fruits = ["🍉", "🍌", "🍇", "🍑", "🥝"]
+
     var body: some View {
-        Text("Hello, world!").padding()
+        NavigationView {
+            Form {
+                DisclosureGroup(
+                    isExpanded: $animalsExpanded,
+                    content: {
+                        HStack {
+                            ForEach(animals, id: \.self) { animal in
+                                Text(animal)
+                                    .font(.system(size: 50))
+                            }
+                        }
+                    },
+                    label: { Text("Animals") }
+                )
+
+                DisclosureGroup(
+                    isExpanded: $fruitsExpanded,
+                    content: {
+                        VStack {
+                            ForEach(fruits, id: \.self) { fruit in
+                                Text(fruit)
+                                    .font(.system(size: 50))
+                            }
+                        }
+                    },
+                    label: { Text("Fruits") }
+                )
+            }
+            .navigationTitle("DisclosureGroup")
+        }
     }
 }
 
